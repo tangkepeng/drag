17c58edaa929d0d568bcf7da1ec2aea9ec1fc254
tangkepeng <429062670@qq.com>
2015-10-13 14:24:37 +0800

    all

--- drag.js
+++ drag.js
@@ -1,6 +1,18 @@
-// JavaScript Document
-
-
-function drag(){
-
+function drag(id){
+	var obj = document.getElementById(id);
+	var disX = 0;
+	var disY = 0;
+	obj.onmousedown = function(ev){
+		disX = ev.pageX - obj.offsetLeft;
+		disY = ev.pageY - obj.offsetTop;
+		document.onmousemove = function(ev){
+			obj.style.left = ev.pageX - disX + 'px';
+			obj.style.top = ev.pageY - disY + 'px';
+		};
+		document.onmouseup = function(){
+			document.onmousemove = null;
+			document.onmouseup = null;
+		};
+	}
+	return false;
 }
